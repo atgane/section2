e222d683af99b7e64f99bb14fa1577ac7b7a5ec9
atgane <hyper2012@naver.com>
2023-05-13 15:37:55 +0900

    Init: terraform 추가

---
+++ kind.tf
@@ -0,0 +1,32 @@
+resource "kind_cluster" "default" {
+  name           = "myapp"
+  wait_for_ready = true
+  
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role  = "control-plane"
+
+      extra_port_mappings {
+        container_port = 30080
+        host_port      = 30080
+      }
+
+      extra_port_mappings {
+        container_port = 30443
+        host_port      = 30443
+      }
+    }
+
+    node {
+      role = "worker"
+    }
+    
+    node {
+      role = "worker"
+    }
+  }
+}
